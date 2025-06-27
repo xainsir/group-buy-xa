4a4f6368271aea5220f59300ed741bb3c238e171
弦瑟 <1476427671@qq.com>
2025-06-27 14:06:29 +0800

    拼团交易开发：1-3 for 研发系统设计

---
+++ group-buy-xa-infrastructure/src/main/java/cn/bugstack/infrastructure/dao/po/GroupBuyActivity.java
@@ -0,0 +1,50 @@
+package cn.bugstack.infrastructure.dao.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupBuyActivity {
+
+    private Long id;/** 自增 */
+
+    private Long activityId;/** 活动ID */
+
+    private String activityName;/** 活动名称 */
+
+    private String discountId; /** 折扣ID */
+
+    private Integer groupType;/** 拼团方式（0自动成团、1达成目标拼团） */
+
+    private Integer takeLimitCount; /** 拼团次数限制 */
+
+    private Integer target;/** 拼团目标 */
+
+    private Integer validTime;/** 拼团时长（分钟） */
+
+    private Integer status;/** 活动状态（0创建、1生效、2过期、3废弃） */
+
+    private Date startTime;/** 活动开始时间 */
+
+    private Date endTime;/** 活动结束时间 */
+
+    private String tagId;/** 人群标签规则标识 */
+
+    private String tagScope;/** 人群标签规则范围 */
+
+    private Date createTime;/** 创建时间 */
+
+    private Date updateTime;/** 更新时间 */
+
+    // 缓存标签
+    public static String cacheRedisKey(Long activityId) {
+        return "group_buy_market_cn.bugstack.infrastructure.dao.po.GroupBuyActivity_" + activityId;
+    }
+
+}
---
+++ group-buy-xa-infrastructure/src/main/java/cn/bugstack/infrastructure/dao/po/GroupBuyDiscount.java
@@ -0,0 +1,38 @@
+package cn.bugstack.infrastructure.dao.po;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupBuyDiscount {
+
+    private Long id; // 自增ID
+
+    private String discountId; // 折扣ID
+
+    private String discountName;// 折扣标题
+
+    private String discountDesc;// 折扣描述
+
+    private Integer discountType;// 折扣类型（0:base、1:tag）
+
+    private String marketPlan;// 营销优惠计划（ZJ:直减、MJ:满减、N元购）
+
+    private String marketExpr;// 营销优惠表达式
+
+    private String tagId;// 人群标签，特定优惠限定
+
+    private Date createTime;// 创建时间
+
+    private Date updateTime;// 更新时间
+
+    public static String cacheRedisKey(String discountId) {
+        return "group_buy_market_cn.bugstack.infrastructure.dao.po.GroupBuyDiscount_" + discountId;
+    }
+
+}
